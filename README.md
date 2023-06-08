83447a84d90764137692c2fe16e602419381c7d7
jmalave15 <qmalavejose1@gmail.com>
2023-06-08 16:37:12 -0400

    adding server with middleware and readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Note Taker Starter Code
+# Note Taker
+
+## Description
+
+Application made to write, save and delete any notes. This
+uses express in the backend and it retrieves/saves data from
+a JSON file.
---
+++ server.js
@@ -0,0 +1,20 @@
+const PORT = process.env.PORT || 3001;
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const notes = require('./db/db');
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
