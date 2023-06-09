901a9a674364ff7911e4fe352883aeb4465b0ed1
jmalave15 <qmalavejose1@gmail.com>
2023-06-08 22:36:12 -0400

    set api routes and express also moved folders

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
+// It sets up the Express server and the API routes.
 const PORT = process.env.PORT || 3001;
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-const notes = require('./db/db');
+const api = require('./routes');
 
-function makeNewNote() {
-    const newNote = body;
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return newNote;
-}
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
+app.use(express.static('public'));
+
+//Route that retrieve notes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-
-    const newNote = makeNewNote(req.body, notes);
-
-    res.json(newNote);
+//get route for notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-    console.log(req.params.id);
-    res.json(req.params.id);
+//get route for all other routes "get", "post", "delete"
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+//listen to port
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`App server now listening at  http://localhost:${PORT}`);
 });
